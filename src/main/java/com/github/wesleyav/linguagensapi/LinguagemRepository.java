830d7d70fbae5df567c545487b45b7f5359160c0
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-07-25 10:16:16 -0300

    Adiciona interface LinguagemRepository

---
+++ src/main/java/com/github/wesleyav/linguagensapi/LinguagemRepository.java
@@ -0,0 +1,7 @@
+package com.github.wesleyav.linguagensapi;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LinguagemRepository extends MongoRepository<Linguagem, String> {
+
+}
